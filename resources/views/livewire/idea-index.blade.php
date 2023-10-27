49145fd4436be6298162dddeb957bd203d275e33
Suliman-000 <sulimna.gt7@gmail.com>
2023-10-27 23:18:08 +0200

    Check If Idea Voted For

--- resources/views/livewire/idea-index.blade.php
+++ resources/views/livewire/idea-index.blade.php
@@ -13,12 +13,16 @@
     class="idea-container hover:shadow-card cursor-pointer transition duration-150 ease-in bg-white rounded-xl flex">
     <div class="hidden md:block border-r border-gray-100 px-5 py-8">
         <div class="text-center">
-            <div class="font-semibold text-2xl">{{$votesCount}}</div>
+            <div class="font-semibold text-2xl @if($hasVoted) text-blue @endif">{{$votesCount}}</div>
             <div class="text-gray-500">Votes</div>
         </div>
 
         <div class="mt-8">
-            <button class="w-20 bg-gray-200 border border-gray-200 hover:border-gray-400 transition duration-150 ease-in font-bold text-xxs uppercase rounded-xl px-4 py-3">Vote</button>
+            @if($hasVoted)
+                <button class="w-20 bg-blue border border-blue hover:bg-blue-hover transition duration-150 ease-in font-bold text-xxs uppercase text-white rounded-xl px-4 py-3">Voted</button>
+            @else
+                <button class="w-20 bg-gray-200 border border-gray-200 hover:border-gray-400 transition duration-150 ease-in font-bold text-xxs uppercase rounded-xl px-4 py-3">Vote</button>
+            @endif
         </div>
     </div>
     <div class="flex flex-col md:flex-row flex-1 px-2 py-6">
@@ -64,11 +68,21 @@
 
                 <div class="flex items-center md:hidden mt-4 md:mt-0">
                     <div class="bg-gray-100 text-center rounded-xl h-10 px-4 py-2 pr-8">
-                        <div class="text-sm font-bold leading-none">{{$votesCount}}</div>
+                        <div class="text-sm font-bold leading-none @if($hasVoted) text-blue @endif">{{$votesCount}}</div>
                         <div class="text-xxs font-semibold leading-none text-gray-400">Votes</div>
                     </div>
-                    <button class="w-20 bg-gray-200 border border-gray-200 hover:border-gray-400 transition duration-150 ease-in font-bold text-xxs uppercase rounded-xl px-4 py-3 -mx-5">Vote</button>
+                    @if($hasVoted)
+                        <button class="w-20 bg-blue border border-blue hover:bg-blue-hover transition duration-150 ease-in font-bold text-xxs uppercase text-white rounded-xl px-4 py-3 -mx-5">Voted</button>
+                    @else
+                        <button class="w-20 bg-gray-200 border border-gray-200 hover:border-gray-400 transition duration-150 ease-in font-bold text-xxs uppercase rounded-xl px-4 py-3 -mx-5">Vote</button>
+                    @endif
                 </div>
+
+                {{-- these divs are nneded to load the colors in the view otherwise it wont show the colors --}}
+                <div class="bg-red hidden"></div>
+                <div class="bg-yellow hidden"></div>
+                <div class="bg-purple hidden"></div>
+                <div class="bg-green hidden"></div>
             </div>
         </div>
     </div>
