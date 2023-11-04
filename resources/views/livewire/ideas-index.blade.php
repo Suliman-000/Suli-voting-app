197b2eb8b216587b3c65d0a2961762bbd49d775f
Suliman-000 <sulimna.gt7@gmail.com>
2023-11-05 00:32:51 +0100

    Search Filter

--- resources/views/livewire/ideas-index.blade.php
+++ resources/views/livewire/ideas-index.blade.php
@@ -16,7 +16,7 @@
             </select>
         </div>
         <div class="w-full md:w-2/3 relative">
-            <input type="search" placeholder="Find an idea" class="w-full rounded-xl border-none bg-white placeholder-gray-900 px-4 py-2 pl-8">
+            <input wire:model.live="search" type="search" placeholder="Find an idea" class="w-full rounded-xl border-none bg-white placeholder-gray-900 px-4 py-2 pl-8">
             <div class="absolute top-0 flex items-center h-full ml-2">
                 <svg class="w-4 text-gray-700" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
@@ -26,9 +26,15 @@
     </div> {{-- end filters --}}
 
     <div class="ideas-container space-y-6 my-6">
-        @foreach ($ideas as $idea)
+
+        @forelse ($ideas as $idea)
             <livewire:idea-index :key="$idea->id" :idea="$idea" :votesCount="$idea->votes_count" />
-        @endforeach
+            @empty
+            <div class="mx-auto w-70 mt-12">
+                <img src="{{ asset('img/notfound.svg') }}" alt="not found image" class="mx-auto w-32 h-32" style="mix-blend-mode: luminosity">
+                <div class="text-gray-400 text-center font-bold mt-6">No Ideas were found...</div>
+            </div>
+        @endforelse
 
     </div> {{-- End ideas container --}}
 
